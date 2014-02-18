d4e2a8b84fb9436b8b44e07a4d98308c3724d8d1
Justin <Justin@Justin-PC>
2014-02-17 18:54:42 -0600

    factored out pull listener, reconstrcuted a flater layout with disappearing menus (top, left, bottom) based on pull directions (thorsen1, lato2)

--- gen/com/macieklato/ragetracks/R.java
+++ gen/com/macieklato/ragetracks/R.java
@@ -25,10 +25,14 @@ public final class R {
         public static final int krewella=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080007;
+        public static final int bottommenu=0x7f080003;
         public static final int gridview=0x7f080000;
-        public static final int picture=0x7f080001;
-        public static final int text=0x7f080002;
+        public static final int leftmenu=0x7f080004;
+        public static final int nowplaying=0x7f080002;
+        public static final int picture=0x7f080005;
+        public static final int text=0x7f080006;
+        public static final int topmenu=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/macieklato/ragetracks/activity/MainActivity.java
+++ src/com/macieklato/ragetracks/activity/MainActivity.java
@@ -2,19 +2,60 @@ package com.macieklato.ragetracks.activity;
 
 import com.macieklato.ragetracks.R;
 import com.macieklato.ragetracks.widget.MyAdapter;
+import com.macieklato.ragetracks.widget.OnPullListener;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.widget.GridView;
 
 public class MainActivity extends Activity {
 
+	OnPullListener menuHider;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
         setContentView(R.layout.activity_main);
         GridView gridView = (GridView)findViewById(R.id.gridview);
+        gridView.setOnTouchListener(menuHider);
         gridView.setAdapter(new MyAdapter(this));
+    }
+    
+    private void init() {
+    	menuHider = new OnPullListener(this.getApplicationContext()) {
+    		@Override
+    		public void onPullUp(View v) {	
+    			findViewById(R.id.topmenu).setVisibility(View.GONE);
+    			findViewById(R.id.nowplaying).setVisibility(View.GONE);
+    			findViewById(R.id.bottommenu).setVisibility(View.VISIBLE);
+    		}
+
+    		@Override
+    		public void onPullDown(View v) {	
+    			findViewById(R.id.topmenu).setVisibility(View.VISIBLE);
+    			findViewById(R.id.nowplaying).setVisibility(View.VISIBLE);
+    			findViewById(R.id.bottommenu).setVisibility(View.GONE);
+    		}
+
+    		@Override
+    		public void onPullLeft(View v) {
+    			findViewById(R.id.leftmenu).setVisibility(View.GONE);
+    		}
+
+    		@Override
+    		public void onPullRight(View v) {
+    			findViewById(R.id.leftmenu).setVisibility(View.VISIBLE);
+    		}
+    		
+    		@Override
+    		public void onRelease(View v){
+    			findViewById(R.id.topmenu).setVisibility(View.VISIBLE);
+    			findViewById(R.id.nowplaying).setVisibility(View.VISIBLE);
+    			findViewById(R.id.bottommenu).setVisibility(View.VISIBLE);
+    		}
+    	};
     }
     
 }
