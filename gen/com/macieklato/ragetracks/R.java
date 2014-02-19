d099b3f25f6a77e52ff225524516e5d7cd9ddc8c
Justin <Justin@192.168.2.7>
2014-02-18 22:13:10 -0600

    basic art and menu controlls (thorsen1, sliu19)

--- gen/com/macieklato/ragetracks/R.java
+++ gen/com/macieklato/ragetracks/R.java
@@ -21,31 +21,54 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int krewella=0x7f020001;
+        public static final int bookmark=0x7f020000;
+        public static final int eject=0x7f020001;
+        public static final int headphones=0x7f020002;
+        public static final int krewella=0x7f020003;
+        public static final int library=0x7f020004;
+        public static final int microphone=0x7f020005;
+        public static final int music=0x7f020006;
+        public static final int next=0x7f020007;
+        public static final int nextnext=0x7f020008;
+        public static final int pause=0x7f020009;
+        public static final int play=0x7f02000a;
+        public static final int previous=0x7f02000b;
+        public static final int previousprevious=0x7f02000c;
+        public static final int rage=0x7f02000d;
+        public static final int repeat=0x7f02000e;
+        public static final int share=0x7f02000f;
+        public static final int stop=0x7f020010;
+        public static final int volumehigh=0x7f020011;
+        public static final int volumelow=0x7f020012;
+        public static final int volumenone=0x7f020013;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int bottommenu=0x7f080003;
-        public static final int gridview=0x7f080000;
-        public static final int leftmenu=0x7f080004;
-        public static final int nowplaying=0x7f080002;
-        public static final int picture=0x7f080005;
-        public static final int text=0x7f080006;
-        public static final int topmenu=0x7f080001;
+        public static final int bookmark_button=0x7f07000a;
+        public static final int bottommenu=0x7f070009;
+        public static final int gridview=0x7f070007;
+        public static final int leftmenu=0x7f070001;
+        public static final int main_screen=0x7f070000;
+        public static final int menu_button=0x7f070004;
+        public static final int next_button=0x7f07000d;
+        public static final int nowplaying=0x7f070008;
+        public static final int picture=0x7f07000f;
+        public static final int play_pause_button=0x7f07000c;
+        public static final int previous_button=0x7f07000b;
+        public static final int right_container=0x7f070006;
+        public static final int right_menu=0x7f070002;
+        public static final int search=0x7f070005;
+        public static final int share_button=0x7f07000e;
+        public static final int text=0x7f070010;
+        public static final int topmenu=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int grid_item=0x7f030001;
     }
-    public static final class menu {
-        public static final int main=0x7f070000;
-    }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int app_id=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int yolo_string=0x7f050003;
+        public static final int nowplaying=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/macieklato/ragetracks/activity/MainActivity.java
+++ src/com/macieklato/ragetracks/activity/MainActivity.java
@@ -7,11 +7,20 @@ import com.macieklato.ragetracks.widget.OnPullListener;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	public static final int PLAY = 0;
+	public static final int PAUSE = 1;
+	
 	OnPullListener menuHider;
+	int state = PAUSE;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +49,10 @@ public class MainActivity extends Activity {
     		}
 
     		@Override
-    		public void onPullLeft(View v) {
-    			findViewById(R.id.leftmenu).setVisibility(View.GONE);
-    		}
+    		public void onPullLeft(View v) {}
 
     		@Override
-    		public void onPullRight(View v) {
-    			findViewById(R.id.leftmenu).setVisibility(View.VISIBLE);
-    		}
+    		public void onPullRight(View v) {}
     		
     		@Override
     		public void onRelease(View v){
@@ -57,5 +62,58 @@ public class MainActivity extends Activity {
     		}
     	};
     }
+    
+    public void onMenuClicked(View v){
+    	Toast.makeText(this.getApplicationContext(), "You clicked menu", Toast.LENGTH_SHORT).show();
+    	View v2 = findViewById(R.id.leftmenu);
+    	LinearLayout v3 = (LinearLayout) findViewById(R.id.main_screen);
+		LayoutParams p = (FrameLayout.LayoutParams) v3.getLayoutParams();
+    	if(v2.getVisibility() == View.VISIBLE) {
+    		v2.setVisibility(View.GONE);
+    		p.rightMargin = 0;
+    		v3.setLayoutParams(p);
+    	} 
+    	else {
+    		p.rightMargin = -v2.getWidth();
+    		v3.setLayoutParams(p);
+    		v2.setVisibility(View.VISIBLE);
+    	}
+    }
+    
+    public void onSearchClicked(View v){
+    	Toast.makeText(this.getApplicationContext(), "You clicked next", Toast.LENGTH_SHORT).show();
+    }
+    
+    public void onBookmarkClicked(View v){
+    	Toast.makeText(this.getApplicationContext(), "You clicked bookmark", Toast.LENGTH_SHORT).show();
+    }
+    
+    public void onPreviousClicked(View v){
+    	Toast.makeText(this.getApplicationContext(), "You clicked previous", Toast.LENGTH_SHORT).show();
+    }
+    
+    public void onPlayPauseClicked(View v){
+    	if(state == PLAY){
+    		Toast.makeText(this.getApplicationContext(), "You clicked pause", Toast.LENGTH_SHORT).show();
+    		ImageView img = (ImageView) findViewById(R.id.play_pause_button);
+    		img.setImageResource(R.drawable.pause);
+    		state = PAUSE;
+    	} else {
+    		Toast.makeText(this.getApplicationContext(), "You clicked play", Toast.LENGTH_SHORT).show();
+    		ImageView img = (ImageView) findViewById(R.id.play_pause_button);
+    		img.setImageResource(R.drawable.play);
+    		state = PLAY;
+    	}
+    }
+    
+    public void onNextClicked(View v){
+    	Toast.makeText(this.getApplicationContext(), "You clicked next", Toast.LENGTH_SHORT).show();
+    }
+    
+    public void onShareClicked(View v){
+    	Toast.makeText(this.getApplicationContext(), "You clicked share", Toast.LENGTH_SHORT).show();
+    }
+    
+    
     
 }
